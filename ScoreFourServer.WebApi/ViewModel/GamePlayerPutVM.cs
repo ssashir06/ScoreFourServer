d271e411fb929f135c341e066ead3f36e2c7da6a
TAKEDA Hiroshi <takejaponpon+git@gmail.com>
2020-04-05 22:57:08 +0900

    Set string length validation

--- ScoreFourServer.WebApi/ViewModel/GamePlayerPutVM.cs
+++ ScoreFourServer.WebApi/ViewModel/GamePlayerPutVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace ScoreFourServer.WebApi.ViewModel
     public class GamePlayerPutVM
     {
         public Guid GameUserId { get; set; }
+        [MaxLength(30), MinLength(1)]
         public string Name { get; set; }
     }
 }
