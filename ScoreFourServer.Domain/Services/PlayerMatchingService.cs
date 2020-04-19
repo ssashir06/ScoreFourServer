a36aeffcf393db56a8a0d13f847d8c32a301ac8c
TAKEDA Hiroshi <takejaponpon+git@gmail.com>
2020-04-19 21:16:15 +0900

    Fix matching logic

--- ScoreFourServer.Domain/Services/PlayerMatchingService.cs
+++ ScoreFourServer.Domain/Services/PlayerMatchingService.cs
@@ -34,9 +34,9 @@ namespace ScoreFourServer.Domain.Services
         public async Task AddGamePlayer(Client player, CancellationToken cancellationToken)
         {
             var waitingPlayer = await this.waitingPlayerAdapter.DequeueAsync(cancellationToken);
-            if (waitingPlayer == null || waitingPlayer.ClientId == player.ClientId)
+            if (waitingPlayer == null || waitingPlayer.GameUserId == player.GameUserId)
             {
-                await waitingPlayerAdapter.EnqueueAsync(player, DateTimeOffset.UtcNow + TimeSpan.FromMinutes(10), cancellationToken);
+                await waitingPlayerAdapter.EnqueueAsync(player, DateTimeOffset.UtcNow + TimeSpan.FromSeconds(30), cancellationToken);
             }
             else
             {
